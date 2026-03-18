04f969e2ba314f83a4005cf2d988b2e1bd601c58
loekemeyer <loekemeyer.n8n@gmail.com>
2026-03-18 11:47:03 -0300

    Update Envios.js

--- Envios.js
+++ Envios.js
@@ -672,26 +672,12 @@ async function enviarCambios(){
   
 console.log("Registros a insertar:", registros);
 
-console.log("Registros a insertar:", registros);
-
 const { error } = await supabaseClient
   .from("Envios a Talleristas")
   .insert(registros);
-
-if (error){
-  console.error("Error al enviar:", error);
-
-  alert(
-    "Error al guardar en Supabase:\n" +
-    (error.message || "") +
-    (error.details ? "\nDetalles: " + error.details : "") +
-    (error.hint ? "\nHint: " + error.hint : "")
-  );
-
-  btnEnviarCambios.disabled = false;
-  btnEnviarCambios.textContent = "Enviar";
-  return;
-}
+  const { error } = await supabaseClient
+    .from("Envios a Talleristas")
+    .insert(registros);
 
 if (error){
   console.error("Error al enviar:", error);
@@ -929,5 +915,4 @@ async function buscar(nombreParam){
 document.addEventListener("DOMContentLoaded", () => {
   cargarTalleristas();
 });
-
 
